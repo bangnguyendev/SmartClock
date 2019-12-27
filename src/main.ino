b169475bff8bed250c07681d5f267c7ceeea1e86
duybang140494 <duybang140494@gmail.com>
2019-12-27 11:30:25 +0700

    NDB

--- src/main.ino
+++ src/main.ino
@@ -1,5 +1,5 @@
-#include "LiquidCrystal_I2C.h"
-#include "D:\Git_NDB\smart_clock_ndb\include\THUVIEN_LCD.H"
+#include "D:\Git_NDB\smart_clock_ndb\include\LiquidCrystal_I2C.cpp"
+#include "D:\Git_NDB\smart_clock_ndb\include\Character_LCD.H"
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h> // http web access library
 #include "time.h"
