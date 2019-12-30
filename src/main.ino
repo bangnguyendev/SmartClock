df710050c459c0f6105b2722b88efc2cd4f90dc4
duybang140494 <duybang140494@gmail.com>
2019-12-30 12:54:47 +0700

    Delete comment <time>
    Delete comment <time>

--- src/main.ino
+++ src/main.ino
@@ -62,32 +62,6 @@ String Location = "1566083";
 /* TK duybang14****@gmail.com*/
 String API_Key = "15fc2e6f826c6eac50fade5e8ebcaeab";
 
-/*
-  %a Abbreviated weekday name 
-  %A Full weekday name 
-  %b Abbreviated month name 
-  %B Full month name 
-  %c Date and time representation for your locale 
-  %d Day of month as a decimal number (01-31) 
-  %H Hour in 24-hour format (00-23) 
-  %I Hour in 12-hour format (01-12) 
-  %j Day of year as decimal number (001-366) 
-  %m Month as decimal number (01-12) 
-  %M Minute as decimal number (00-59) 
-  %p Current locale's A.M./P.M. indicator for 12-hour clock 
-  %S Second as decimal number (00-59) 
-  %U Week of year as decimal number,  Sunday as first day of week (00-51) 
-  %w Weekday as decimal number (0-6; Sunday is 0) 
-  %W Week of year as decimal number, Monday as first day of week (00-51) 
-  %x Date representation for current locale 
-  %X Time representation for current locale 
-  %y Year without century, as decimal number (00-99) 
-  %Y Year with century, as decimal number 
-  %z %Z Time-zone name or abbreviation, (no characters if time zone is unknown) 
-  %% Percent sign 
-  You can include text literals (such as spaces and colons) to make a neater display or for padding between adjoining columns. 
-  You can suppress the display of leading zeroes  by using the "#" character  (%#d, %#H, %#I, %#j, %#m, %#M, %#S, %#U, %#w, %#W, %#y, %#Y) 
-*/
 char buffer_NAM[80];
 char buffer_THANG[80];
 char buffer_NGAY[80];
@@ -514,6 +488,7 @@ void kiem_tra_nut_nhan()
 					lcd.setCursor(0, 1);
 					lcd.print("Mode Selection      ");
 				}
+				/* vao mode Message */
 				else if (couter_mode >= 7)
 				{
 					lcd.setCursor(0, 1);
@@ -663,6 +638,7 @@ void kiem_tra_nut_nhan()
 	}
 }
 
+/* Chon vi tri doc gia tri thoi tiet */
 void Choose_location()
 {
 	lcd.clear();
